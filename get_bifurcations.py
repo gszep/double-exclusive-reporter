65838206adc442474e4cad971fb64ea82d8045f6
gszep <gregory.szep@gmail.com>
2019-08-15 19:00:24 +0100

    optionally show other datasets

--- get_bifurcations.py
+++ get_bifurcations.py
@@ -15,22 +15,25 @@ def get_args() :
 	'''parse arguments from command line'''
 	parser = ArgumentParser(description='creates bifrucation plot from crn file parameters')
 
-	parser.add_argument('crn_path', type=str,
-						help='path to crn file')
+	parser.add_argument('crn_path', type=str, help='path to crn file')
+	parser.add_argument('--model', type=bool, default=False, help='model predictions')
+	parser.add_argument('--data_path', type=str, default='./data/liquid/char_ExRep_1_R33S175ExRepTet33AAVLac300ND.csv',help='liquid culture dataset')
 	return vars(parser.parse_args())
 
 
-def get_bifurcations(crn_path,file='char_ExRep_1',device='R33S175ExRepTet33AAVLac300ND'):
+def get_bifurcations(crn_path,model,data_path):
 	'''Calculate bifrucation diagram for double exclusive reporter
 	for a given range of diffusives c6 and c12'''
 
-	parameters.update(crn_parameters(crn_path))
-	model = Model(pars = parameters , **system_specifications)
+	if model :
 
-	model.integrate()
-	model.get_cusp('c6','c12')
+		parameters.update(crn_parameters(crn_path))
+		model = Model(pars = parameters , **system_specifications)
+
+		model.integrate()
+		model.get_cusp('c6','c12')
 	
-	liquid_data = read_csv('./data/liquid/{}_{}.csv'.format(file,device))
+	liquid_data = read_csv(data_path)
 	cfp,yfp = liquid_data.pivot('C6','C12','P(ECFP/mRFP1)'), liquid_data.pivot('C6','C12','P(EYFP/mRFP1)')
 	c12,c6 = meshgrid(cfp.columns.values,cfp.index.values)
 
@@ -48,9 +51,10 @@ def generate_figure(model,c6,c12,cfp,yfp):
 	pcolor(c12,c6,log10(yfp.values),cmap='yellow',vmin=1,vmax=2.1)
 	colorbar(ticks=[]).ax.set_yticklabels([])
 	
-	region = model.bifurcations['LC1']
-	region_c6,region_c12 = region.curve[:-1,region.params].T
-	fill_between(10**region_c12,10**region_c6,facecolor="none",hatch="///", edgecolor="k",linewidth=0)
+	if model :
+		region = model.bifurcations['LC1']
+		region_c6,region_c12 = region.curve[:-1,region.params].T
+		fill_between(10**region_c12,10**region_c6,facecolor="none",hatch="///", edgecolor="k",linewidth=0)
 	
 	yscale('log'); xscale('log')
 	xlim(0.04,2500); ylim(0.04,2500)
@@ -65,10 +69,10 @@ def generate_figure(model,c6,c12,cfp,yfp):
 	show()
 
 
-def main(crn_path) :
+def main(crn_path,model,data_path) :
 	'''parametrisation of main program'''
 
-	model,c6,c12,cfp,yfp = get_bifurcations(crn_path)
+	model,c6,c12,cfp,yfp = get_bifurcations(crn_path,model,data_path)
 	generate_figure(model,c6,c12,cfp,yfp)
 
 
